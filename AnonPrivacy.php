29e5a389a61f2a076eddc84a4ea50ef7cbce72cf
Garsooon <carson12345678910@hotmail.com>
2025-05-17 17:52:35 -0700

    Array implementation prototype, sorry I suck at php

--- AnonPrivacy.php
+++ AnonPrivacy.php
@@ -15,40 +15,44 @@ class AnonPrivacy {
 	 * @return bool
 	 */
 	public static function onHtmlPageLinkRendererEnd( $linkRenderer, $target, $isKnown, &$text, &$attribs, &$ret ) {
-		// MW_NO_SESSION happens if we are called from load.php, in which
-		// case we can not check user permissions, so fail safe. This is known
-		// to happen with VisualEditor.
-		if (
-			!defined( 'MW_NO_SESSION' ) &&
-			RequestContext::getMain()->getUser()->isAllowed( 'anonprivacy' )
-		) {
-			return true;
-		}
+        // MW_NO_SESSION happens if we are called from load.php, in which
+        // case we can not check user permissions, so fail safe. This is known
+        // to happen with VisualEditor.
+        if (
+            !defined( 'MW_NO_SESSION' ) &&
+            RequestContext::getMain()->getUser()->isAllowed( 'anonprivacy' )
+        ) {
+            return true;
+        }
 
-		if ( array_key_exists( 'class', $attribs ) ) {
-			$class = $attribs['class'];
-		} else {
-			return true;
-		}
+        // Ensure $text is a string (convert array to string if needed)
+        $text = is_array($text) ? implode(' ', $text) : $text;
 
-		// Check if we're dealing with a link to an anonymous user page
-		if ( strpos( $class, 'mw-anonuserlink' ) > -1 ) {
-			$privacypage = wfMessage( 'privacypage' )->plain();
-			$privacytitle = Title::newFromText( $privacypage );
-			$attribs['href'] = $privacytitle->getInternalURL();
-			$attribs['class'] = 'mw-userlink mw-anonuserlink';
-			$attribs['title'] = wfMessage( 'privacy' )->text();
-			$text = wfMessage( 'anonprivacy-anon' )->text();
-		}
+        if ( array_key_exists( 'class', $attribs ) ) {
+            $class = $attribs['class'];
+        } else {
+            return true;
+        }
 
-		// Check if we're dealing with a link to an anonymous user talk page
-		if ( strpos( $class, 'mw-usertoollinks' ) > -1 && $target->getNamespace() === NS_USER_TALK && IPUtils::isIPAddress( $target->getText() ) ) {
-			$privacypage = wfMessage( 'privacypage' )->plain();
-			$privacytitle = Title::newFromText( $privacypage );
-			$attribs['href'] = $privacytitle->getInternalURL();
-			$attribs['class'] = 'mw-usertoollinks-talk';
-			$attribs['title'] = wfMessage( 'privacy' )->text();
-			$text = wfMessage( 'privacy' )->text();
-		}
-	}
-}
+        // Check if we're dealing with a link to an anonymous user page
+        if ( strpos( $class, 'mw-anonuserlink' ) !== false ) {
+            $privacypage = wfMessage( 'privacypage' )->plain();
+            $privacytitle = Title::newFromText( $privacypage );
+            $attribs['href'] = $privacytitle->getInternalURL();
+            $attribs['class'] = 'mw-userlink mw-anonuserlink';
+            $attribs['title'] = wfMessage( 'privacy' )->text();
+            $text = wfMessage( 'anonprivacy-anon' )->text();
+        }
+
+        // Check if we're dealing with a link to an anonymous user talk page
+        if ( strpos( $class, 'mw-usertoollinks' ) !== false && $target->getNamespace() === NS_USER_TALK && IPUtils::isIPAddress( $target->getText() ) ) {
+            $privacypage = wfMessage( 'privacypage' )->plain();
+            $privacytitle = Title::newFromText( $privacypage );
+            $attribs['href'] = $privacytitle->getInternalURL();
+            $attribs['class'] = 'mw-usertoollinks-talk';
+            $attribs['title'] = wfMessage( 'privacy' )->text();
+            $text = wfMessage( 'privacy' )->text();
+        }
+
+        return true;
+    }
